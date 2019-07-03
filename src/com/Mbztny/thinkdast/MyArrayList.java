3dac79ebe3f6d887a4c3b822f43addbcb636232e
ckookc3 <2290438294@qq.com>
2019-07-03 13:38:10 +0800

    source

--- src/com/Mbztny/thinkdast/MyArrayList.java
+++ src/com/Mbztny/thinkdast/MyArrayList.java
@@ -196,7 +196,12 @@ public class MyArrayList<T> implements List<T> {
 	@Override
 	public T remove(int index) {
 		// TODO: FILL THIS IN!
-		return null;
+		T element = get(index);
+		for (int i = index; i < size - 1; i++) {
+			array[i] = array[i + 1];
+		}
+		size--;
+		return element;
 	}
 
 	@Override
